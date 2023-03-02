2833da7b0f8ffded71873dbe3f0370c3d18df339
davidsmenesesc <david86543@hotmail.com>
2023-03-02 08:38:08 -0500

    first commit

---
+++ providers.tf
@@ -0,0 +1,6 @@
+    # provider "google" {
+    # credentials = file("tf_demo_auth.json")
+    # project = var.project_id
+    # region  = var.region
+    # zone    = var.zone
+    # }
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  description = "The region the environment is going to be installed into"
+  type        = string
+  default     = "us-east1"
+}
+variable "zone" {
+  description = "The region the environment is going to be installed into"
+  type        = string
+  default     = "us-east1-b"
+}
+variable "project_id" {
+  description = "The region the environment is going to be installed into"
+  type        = string
+  default     = "keen-sight-378515"
+}
